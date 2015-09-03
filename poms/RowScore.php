9c520745440218e6a6ec2987735f54caa2b7c515
Flávio Micheletti <flaviomicheletti@hotmail.com>
2015-09-02 23:56:16 -0300

    commit incial

---
+++ poms/RowScore.php
@@ -0,0 +1,145 @@
+<?php
+
+/**
+ * Classe que abstrai a pontuação row-score do método Poms
+ */
+class RowScore {
+
+    public $vigor     = 0;
+    public $tensao    = 0;
+    public $raiva     = 0;
+    public $confusao  = 0;
+    public $fadiga    = 0;
+    public $depressao = 0;
+
+    /**
+     * O calculo consiste em somar o valor escolhido no formulário
+     * A exceção é para as alternativas 22 e 54
+     * 
+     * @param type $id
+     * @param type $fator
+     * @param type $valor
+     * @return type
+     */
+    function calcularValor($id, $fator, $valor) {
+        if($id == 22 || $id == 54){
+            return $this->subtrairSomaQuatro($fator, $valor);
+        } else {
+            return $this->somar($fator, $valor);
+        }
+    }    
+    
+    public function somar($fator, $valor) {
+        return $this->$fator + $valor;
+    }
+
+    /**
+     * Subtrai o valor e soma 4
+     * 
+     * @param type $fator
+     * @param type $valor
+     * @return type
+     */
+    public function subtrairSomaQuatro($fator, $valor) {
+        return $this->$fator - $valor + 4;
+    }
+
+    /**
+     * O valor escolhido no formulário deve ser subtraído
+     * um unidade.
+     * 
+     * entrada | saída
+     *    1        0
+     *    2        1
+     *    3        2
+     *    4        3
+     *    5        4
+     * 
+     * @param type $valor
+     * @return type
+     */
+    public function corrigirValor($valor) {
+        return $valor - 1;
+    }
+
+    /**
+     * Eis a relação das alternativas e os fatores
+     * A 'key' deve ser equivalente ao formulário
+     * 
+     * @param type $key
+     * @return string
+     */
+    public function retFator($key) {
+        $relacao = array(
+            0 => "",
+            1 => "vigor",
+            2 => "tensao",
+            3 => "raiva",
+            4 => "fadiga",
+            5 => "depressao",
+            6 => "vigor",
+            7 => "vigor",
+            8 => "confusao",
+            9 => "depressao",
+            10 => "tensao",
+            11 => "fadiga",
+            12 => "raiva",
+            13 => "tensao",
+            14 => "depressao",
+            15 => "vigor",
+            16 => "tensao",
+            17 => "raiva",
+            18 => "depressao",
+            19 => "vigor",
+            20 => "tensao",
+            21 => "depressao",
+            22 => "tensao",
+            23 => "depressao",
+            24 => "raiva",
+            25 => "vigor",
+            26 => "tensao",
+            27 => "tensao",
+            28 => "confusao",
+            29 => "fadiga",
+            30 => "vigor",
+            31 => "raiva",
+            32 => "depressao",
+            33 => "raiva",
+            34 => "tensao",
+            35 => "depressao",
+            36 => "depressao",
+            37 => "confusao",
+            38 => "vigor",
+            39 => "raiva",
+            40 => "fadiga",
+            41 => "tensao",
+            42 => "raiva",
+            43 => "vigor",
+            44 => "depressao",
+            45 => "depressao",
+            46 => "fadiga",
+            47 => "raiva",
+            48 => "depressao",
+            49 => "fadiga",
+            50 => "confusao",
+            51 => "vigor",
+            52 => "raiva",
+            53 => "raiva",
+            54 => "confusao",
+            55 => "vigor",
+            56 => "vigor",
+            57 => "raiva",
+            58 => "depressao",
+            59 => "confusao",
+            60 => "vigor",
+            61 => "depressao",
+            62 => "depressao",
+            63 => "vigor",
+            64 => "confusao",
+            65 => "fadiga"
+        );
+
+        return $relacao[$key];
+    }
+
+}
---
+++ tests/PomsCalcTest.php
@@ -0,0 +1,128 @@
+<?php
+
+require "../poms/Calc.php";
+require "../poms/RowScore.php";
+
+class CalcTest extends PHPUnit_Framework_TestCase {
+
+    public function testRowScore_CalculoSimples() {
+        $pomsCalc = new Calc();
+
+        $rowScore = $pomsCalc->rowScore("1-5");
+        $this->assertEquals(4, $rowScore->vigor);
+
+        $rowScore = $pomsCalc->rowScore("1-5, 2-5");
+        $this->assertEquals(4, $rowScore->vigor);
+        $this->assertEquals(4, $rowScore->tensao);
+
+        $rowScore = $pomsCalc->rowScore("2-4");
+        $this->assertEquals(3, $rowScore->tensao);
+
+        $rowScore = $pomsCalc->rowScore("3-3");
+        $this->assertEquals(2, $rowScore->raiva);
+
+        $rowScore = $pomsCalc->rowScore("11-1");
+        $this->assertEquals(0, $rowScore->fadiga);
+        
+        $rowScore = $pomsCalc->rowScore("21-2");
+        $this->assertEquals(1, $rowScore->depressao);
+        
+    }
+
+    /** 
+     * Alternativa 22 soma de forma diferente
+     * 
+     * Executa `subtrairSomaQuatro()`
+     */
+    public function testRowScore_Alternativa22SomaDiferente() {
+        $pomsCalc = new Calc();
+
+        $rowScore = $pomsCalc->rowScore("22-5");
+        # 0 - (5 - 1) + 4
+        $this->assertEquals(0, $rowScore->tensao); 
+
+        $rowScore = $pomsCalc->rowScore("22-4");
+        # 0 - (4 - 1) + 4
+        $this->assertEquals(1, $rowScore->tensao);
+        
+        $rowScore = $pomsCalc->rowScore("22-3");
+        # 0 - (3 - 1) + 4
+        $this->assertEquals(2, $rowScore->tensao);
+        
+        $rowScore = $pomsCalc->rowScore("22-2");
+        # 0 - (2 - 1) + 4
+        $this->assertEquals(3, $rowScore->tensao);
+
+        $rowScore = $pomsCalc->rowScore("22-1");
+        # 0 - (1 - 1) + 4
+        $this->assertEquals(4, $rowScore->tensao);
+    }
+    
+    /** 
+     * Alternativa 54 soma de forma diferente
+     * 
+     * Executa `subtrairSomaQuatro()`
+     * 
+     */
+    public function testRowScore_Alternativa54SomaDiferente() {
+        $pomsCalc = new Calc();
+
+        $rowScore = $pomsCalc->rowScore("54-5");
+        # 0 - (5 - 1) + 4
+        $this->assertEquals(0, $rowScore->confusao); 
+
+        $rowScore = $pomsCalc->rowScore("54-4");
+        # 0 - (4 - 1) + 4
+        $this->assertEquals(1, $rowScore->confusao);
+        
+        $rowScore = $pomsCalc->rowScore("54-3");
+        # 0 - (3 - 1) + 4
+        $this->assertEquals(2, $rowScore->confusao);
+        
+        $rowScore = $pomsCalc->rowScore("54-2");
+        # 0 - (2 - 1) + 4
+        $this->assertEquals(3, $rowScore->confusao);
+
+        $rowScore = $pomsCalc->rowScore("54-1");
+        # 0 - (1 - 1) + 4
+        $this->assertEquals(4, $rowScore->confusao);
+    }
+
+    
+    public function testRetIdFator() {
+        $pomsCalc = new Calc();
+
+        $this->assertEquals(1, $pomsCalc->retIdFator("1-5"));
+        $this->assertEquals(2, $pomsCalc->retIdFator("2-5"));
+        $this->assertEquals(3, $pomsCalc->retIdFator("3-5"));
+        $this->assertEquals(4, $pomsCalc->retIdFator("4-5"));
+        $this->assertEquals(5, $pomsCalc->retIdFator("5-5"));
+
+        $this->assertEquals(99, $pomsCalc->retIdFator("99-5"));
+    }
+
+    /**
+     * Retorna valor real do fator (sem correção)
+     */
+    public function testRetValorFator() {
+        $pomsCalc = new Calc();
+
+        $this->assertEquals(1, $pomsCalc->retValorFator("1-1"));
+        $this->assertEquals(2, $pomsCalc->retValorFator("1-2"));
+        $this->assertEquals(3, $pomsCalc->retValorFator("1-3"));
+        $this->assertEquals(4, $pomsCalc->retValorFator("1-4"));
+        $this->assertEquals(5, $pomsCalc->retValorFator("1-5"));
+        $this->assertEquals(99, $pomsCalc->retValorFator("1-99"));
+    }
+
+    public function testSepararStringFormulario() {
+        $pomsCalc = new Calc();
+
+        $stringFormulario = "1-5, 2-5, 3-5";
+        $arr = $pomsCalc->separarStringFormulario($stringFormulario);
+        $this->assertEquals("1-5", $arr[0]);
+        $this->assertEquals("2-5", $arr[1]);
+        $this->assertEquals("3-5", $arr[2]);
+    }
+
+}
