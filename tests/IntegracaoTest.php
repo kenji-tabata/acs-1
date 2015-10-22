e28fa52b96fcd7eb141fa6125349ddf7644d39d6
Flávio Micheletti <flaviomicheletti@hotmail.com>
2015-10-22 15:49:51 -0200

    test/integracao

--- tests/IntegracaoTest.php
+++ tests/IntegracaoTest.php
@@ -5,4 +5,13 @@ class IntegracaoTest extends PHPUnit_Framework_TestCase {
     public function testSeArquivoDeFontesParaPDfExiste() {
         $this->assertTrue(file_exists("fonts/arial.ttf"));
     }
+
+    public function testSePastaFilesTempExiste() {
+        $this->assertTrue(file_exists("files-temp/"));
+    }
+
+    public function testHaPermissaoDeEscreitaNaPastaFilesTemp() {
+        $this->assertTrue(is_writable("files-temp/"));
+    }
+
 }
