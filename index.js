284f1a7f2f79dd6fdd63850f98681f909627bdbe
Flávio Micheletti <flaviomicheletti@hotmail.com>
2015-11-05 12:51:17 -0200

    index.js - FormularioView()

--- index.js
+++ index.js
@@ -10,25 +10,25 @@ var JumbotronView = Backbone.View.extend({
     template: _.template("<h1><%= titulo %></h1><p><%= paragrafo %></p>"),
     initialize: function () {
         this.render();
-    },    
+    },
     render: function() {
         this.$el.html(this.template(this.model.attributes));
         return this;
     }
 });
 
-var ContentView = Backbone.View.extend({
-    el: $('#content'),
-    //     template: _.template($("#lista-poms-profissionais").html()),
+var ListaView = Backbone.View.extend({
+    tagName: "table",
+    className: "table",
+    template: _.template($("#poms-lista-de-profissionais").html()),
     initialize: function () {
         this.render();
     },
     render: function() {
-        this.$el.html('foi');
+        this.$el.html(this.template({profissionais: []}));
         return this;
-    }    
+    }
 });
-
 
 var Profissional = Backbone.Model.extend({});
 var PomsPreenchidos = new Backbone.Collection(
@@ -40,6 +40,40 @@ var PomsPreenchidos = new Backbone.Collection(
     ]
 );
 
+Formulario = Backbone.Model.extend({
+    urlRoot: '/_acs/poms/formulario/',   
+});
+
+var formulario = new Formulario(
+    {
+//         id: 123,
+        name:      $("#txt-nome").val(),
+        email:     $("#txt-email").val(),
+        cpf:       $("#txt-cpf").val(),
+        genero:    $('input[name=genero]:checked').val(),        
+        adjetivos: "string-separada-por-virgula, ex: 1-1, 2-1, 3-1, etc...",
+        eDepois:   $('input[name=depois-de-salvar]:checked').val(),
+    }
+);
+
+var FormularioView = Backbone.View.extend({
+    tagName: "form",
+    attributes: {
+        "action": "salvar/",
+        "method": "post"
+    },    
+    template: _.template( $("#poms-formulario").html()),
+    initialize: function(){
+        this.render();
+    },
+    render: function(){
+         this.$el.html(this.template);
+    },
+    events: {
+        "click #btn-salvar": "salvar"
+    }
+});
+
 
 var AppRouter = Backbone.Router.extend({
     routes: {
@@ -49,29 +83,38 @@ var AppRouter = Backbone.Router.extend({
         'poms-formulario/:id': 'abrir_formulario_poms',
     },
     index: function () {
-        console.log('index');
+        console.log('index()');
+        var jumbo_model = new JumbotronModel(
+            {
+                'titulo':    "Sistemas ACS",
+                'paragrafo': "Poms, ACS 1"
+            }
+        );
+        var jumbo_view    = new JumbotronView({'model': jumbo_model});         
     },
     listar_profissionais: function () {
-        console.log('listar_profissionais');
+        console.log('listar_profissionais()');
         var jumbo_model = new JumbotronModel(
             {
                 'titulo':    "POMS",
                 'paragrafo': "Lista de profissionais que preencheram o formulário POMS."
             }
         );
-        var jumbo_view    = new JumbotronView({'model': jumbo_model});        
-        var content_view  = new ContentView({'model': jumbo_model});
-
+        var jumbo_view = new JumbotronView({'model': jumbo_model});        
+        var lista_view = new ListaView();
+        $('#content').html(lista_view.el);
     },
     formulario_poms: function () {
-        console.log('formulario_poms');
+        console.log('formulario_poms()');
         var jumbo_model = new JumbotronModel(
             {
                 'titulo':    "POMS",
                 'paragrafo': "Formulário POMS."
             }
         );
-        var jumbo_view  = new JumbotronView({'model': jumbo_model});        
+        var jumbo_view      = new JumbotronView({'model': jumbo_model});        
+        var formulario_view = new FormularioView({model: formulario});
+        $('#content').html(formulario_view.el);
     },
     abrir_formulario_poms: function (id) {
         console.log('abrir_formulario_poms:' + id);
