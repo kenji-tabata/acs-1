aa783d05de7d9cb3dc6552d32f5e21db86531905
Flávio Micheletti <flaviomicheletti@hotmail.com>
2016-03-02 11:07:22 -0300

    refatorando RelatorioGrupoTest

--- src/tests/PomsRelatorioGrupoTest.php
+++ src/tests/PomsRelatorioGrupoTest.php
@@ -16,56 +16,59 @@ class RelatorioGrupoTest extends PHPUnit_Framework_TestCase {
 
         $this->grupo = [];
 
-        # profissional padrão
+        # profissional 1
         $this->profissional = new Profissional();
-        $this->profissional->nome   = "Fulano";
-        $this->profissional->cpf    = "111.2222.333.45";
-        $this->profissional->email  = "fulano@qualquer.com.br";
+        $this->profissional->nome   = "Fulano 1";
+        $this->profissional->cpf    = "111.222.333.45";
+        $this->profissional->email  = "fulano1@qualquer.com.br";
         $this->profissional->genero = "masculino";
 
-        # profissional 1
-        $rowScore = new RowScore();
-        $rowScore->tensao    = 12;
-        $rowScore->depressao = 20;
-        $rowScore->raiva     = 16;
-        $rowScore->vigor     = 19;
-        $rowScore->fadiga    = 10;
-        $rowScore->confusao  = 8;
+        $this->profissional->rowScore = new RowScore();
+        $this->profissional->rowScore->tensao    = 12;
+        $this->profissional->rowScore->depressao = 20;
+        $this->profissional->rowScore->raiva     = 16;
+        $this->profissional->rowScore->vigor     = 19;
+        $this->profissional->rowScore->fadiga    = 10;
+        $this->profissional->rowScore->confusao  = 8;
 
-        $tScore = new TScore();
-        $tScore->tensao    = 60;
-        $tScore->depressao = 60;
-        $tScore->raiva     = 60;
-        $tScore->vigor     = 60;
-        $tScore->fadiga    = 60;
-        $tScore->confusao  = 60;
+        $this->profissional->tScore = new TScore();
+        $this->profissional->tScore->tensao    = 60;
+        $this->profissional->tScore->depressao = 60;
+        $this->profissional->tScore->raiva     = 60;
+        $this->profissional->tScore->vigor     = 60;
+        $this->profissional->tScore->fadiga    = 60;
+        $this->profissional->tScore->confusao  = 60;
 
-        $this->profissional->rowScore  = $rowScore;
-        $this->profissional->grafico = Grafico::gerar($tScore, $rowScore);
-        $this->profissional->laudo   = Laudos::laudo($tScore);
-        $this->grupo[] = clone $this->profissional;
+        $this->profissional->grafico = Grafico::gerar($this->profissional->tScore, $this->profissional->rowScore);
+        $this->profissional->laudo   = Laudos::laudo($this->profissional->tScore);
+        $this->grupo[] = $this->profissional;
 
         # profissional 2
-        $rowScore = new RowScore();
-        $rowScore->tensao    = 24;
-        $rowScore->depressao = 40;
-        $rowScore->raiva     = 32;
-        $rowScore->vigor     = 37;
-        $rowScore->fadiga    = 20;
-        $rowScore->confusao  = 16;
+        $this->profissional = new Profissional();
+        $this->profissional->nome   = "Fulano 2";
+        $this->profissional->cpf    = "666.777.888.99";
+        $this->profissional->email  = "fulano2@qualquer.com.br";
+        $this->profissional->genero = "feminino";
 
-        $tScore = new TScore();
-        $tScore->tensao    = 80;
-        $tScore->depressao = 80;
-        $tScore->raiva     = 80;
-        $tScore->vigor     = 80;
-        $tScore->fadiga    = 82;
-        $tScore->confusao  = 80;
+        $this->profissional->rowScore = new RowScore();
+        $this->profissional->rowScore->tensao    = 24;
+        $this->profissional->rowScore->depressao = 40;
+        $this->profissional->rowScore->raiva     = 32;
+        $this->profissional->rowScore->vigor     = 37;
+        $this->profissional->rowScore->fadiga    = 20;
+        $this->profissional->rowScore->confusao  = 16;
 
-        $this->profissional->rowScore = $rowScore;
-        $this->profissional->grafico  = Grafico::gerar($tScore, $rowScore);
-        $this->profissional->laudo    = Laudos::laudo($tScore);
-        $this->grupo[] = clone $this->profissional;
+        $this->profissional->tScore = new TScore();
+        $this->profissional->tScore->tensao    = 80;
+        $this->profissional->tScore->depressao = 80;
+        $this->profissional->tScore->raiva     = 80;
+        $this->profissional->tScore->vigor     = 80;
+        $this->profissional->tScore->fadiga    = 82;
+        $this->profissional->tScore->confusao  = 80;
+
+        $this->profissional->grafico  = Grafico::gerar($this->profissional->tScore, $this->profissional->rowScore);
+        $this->profissional->laudo    = Laudos::laudo($this->profissional->tScore);
+        $this->grupo[] = $this->profissional;
     }
 
     public function testGeradorDoRelatorioGrupoPoms() {
@@ -85,7 +88,7 @@ class RelatorioGrupoTest extends PHPUnit_Framework_TestCase {
         $relatorio->gravar();
 
         $this->assertTrue(file_exists($relatorio->getNomeArquivo()));
-        // $relatorio->deletar_relatorio();
+        //$relatorio->deletar_relatorio();
         $grafico->deletar_imagem();
 
     }
