ab72dc8b4b9d22603feefb675c1b5202774f08d2
Flávio Micheletti <flaviomicheletti@hotmail.com>
2016-04-11 15:44:57 -0300

    poms/Grupo

--- src/poms/Grupo.php
+++ src/poms/Grupo.php
@@ -1,4 +1,14 @@
 <?php
+require_once dirname(__FILE__) . "/../poms/Profissional.php";
+require_once dirname(__FILE__) . "/../poms/Grupo.php";
+require_once dirname(__FILE__) . "/../poms/Laudos.php";
+require_once dirname(__FILE__) . "/../poms/Grafico.php";
+require_once dirname(__FILE__) . "/../poms/Calc.php";
+require_once dirname(__FILE__) . "/../poms/TScore.php";
+require_once dirname(__FILE__) . "/../poms/RowScoreMedio.php";
+require_once dirname(__FILE__) . "/../poms/Model.php";
+require_once dirname(__FILE__) . "/../includes/DBpdo.php";
+
 
 class Grupo {
 
@@ -10,6 +20,30 @@ class Grupo {
 
     function get() {
         return $this->profissionais;
+    }
+
+    function factory($ids) {
+        $grupo = new Grupo;
+        foreach ($ids as $id) {
+            $model = new PomsModel();
+            $profissional = $model->read_profissional($id);
+            $profissional->poms     = Calc::perfilPoms($profissional->adjetivos);
+            $profissional->tScore   = $profissional->poms->tScore;
+            $profissional->rowScore = $profissional->poms->rowScore;
+            $profissional->grafico  = Grafico::gerar($profissional->poms->tScore, $profissional->poms->rowScore);
+            $profissional->laudo    = Laudos::laudo($profissional->poms->tScore);
+            $grupo->add($profissional);
+        }
+        // var_dump($grupo);
+
+        $grupo->rowScore = new RowScoreMedio();
+        $grupo->rowScore->calcular($grupo->get());
+        $grupo->tScore = new TScore();
+        $grupo->tScore->converterParaTScore($grupo->rowScore);
+        $grupo->grafico = Grafico::gerar($grupo->tScore, $grupo->rowScore);
+
+        return $grupo;
+
     }
 
 }
