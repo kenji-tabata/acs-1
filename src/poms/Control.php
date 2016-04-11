a57ce65e868b9fc5fe59818a23798bcc6c685588
Flávio Micheletti <flaviomicheletti@hotmail.com>
2016-04-11 15:45:12 -0300

    poms/Control

--- src/poms/Control.php
+++ src/poms/Control.php
@@ -106,41 +106,42 @@ App::$slim->get('/poms/relatorio/:id', function ($id) {
 # Relatório em grupo
 #
 # POST http://localhost/acs/src/poms/relatorio/grupo
-# /poms/relatorio/grupo/[1, 2, 3]
+# /poms/relatorio/grupo/[14, 15, 18]
 App::$slim->get('/poms/relatorio/grupo/:ids', function ($ids) {
-    require "RelatorioGrupo.php";
     require "Grupo.php";    
-    require "Laudos.php";
-    require "Grafico.php";
-    require "Calc.php";
-    require "RowScore.php";
-    require "RowScoreMedio.php";
-    require "TScore.php";
-    require "Model.php";
-    require "includes/DBpdo.php";
+    require "RelatorioGrupo.php";
+    // require "Laudos.php";
+    // require "Grafico.php";
+    // require "Calc.php";
+    // require "RowScore.php";
+    // require "RowScoreMedio.php";
+    // require "TScore.php";
+    // require "Model.php";
+    // require "includes/DBpdo.php";
 
     // var_dump(json_decode($ids));
 
-    $ids = json_decode($ids);
     $grupo = new Grupo;
-    foreach ($ids as $id) {
-        $model = new PomsModel();
-        $profissional = $model->read_profissional($id);
-        $profissional->poms    = Calc::perfilPoms($profissional->adjetivos);
-        $profissional->tScore   = $profissional->poms->tScore;
-        $profissional->rowScore = $profissional->poms->rowScore;
-        $profissional->grafico = Grafico::gerar($profissional->poms->tScore, $profissional->poms->rowScore);
-        $profissional->laudo   = Laudos::laudo($profissional->poms->tScore);
-        $grupo->add($profissional);
-    }
-    // var_dump($grupo);    
+    $grupo = $grupo->factory(json_decode($ids));
+    
+    // foreach ($ids as $id) {
+    //     $model = new PomsModel();
+    //     $profissional = $model->read_profissional($id);
+    //     $profissional->poms     = Calc::perfilPoms($profissional->adjetivos);
+    //     $profissional->tScore   = $profissional->poms->tScore;
+    //     $profissional->rowScore = $profissional->poms->rowScore;
+    //     $profissional->grafico  = Grafico::gerar($profissional->poms->tScore, $profissional->poms->rowScore);
+    //     $profissional->laudo    = Laudos::laudo($profissional->poms->tScore);
+    //     $grupo->add($profissional);
+    // }
+    // // var_dump($grupo);    
 
-    $grupo->rowScore = new RowScoreMedio();
-    $grupo->rowScore->calcular($grupo->get());
-    $grupo->tScore = new TScore();
-    $grupo->tScore->converterParaTScore($grupo->rowScore);
-    $grupo->grafico = Grafico::gerar($grupo->tScore, $grupo->rowScore);
-    // var_dump($grupo);
+    // $grupo->rowScore = new RowScoreMedio();
+    // $grupo->rowScore->calcular($grupo->get());
+    // $grupo->tScore = new TScore();
+    // $grupo->tScore->converterParaTScore($grupo->rowScore);
+    // $grupo->grafico = Grafico::gerar($grupo->tScore, $grupo->rowScore);
+    // // var_dump($grupo);
 
     $relatorio = new RelatorioGrupo($grupo);
     $relatorio->setGrafico($grupo->grafico->getNomeArquivo());
@@ -151,6 +152,48 @@ App::$slim->get('/poms/relatorio/grupo/:ids', function ($ids) {
     // $relatorio->gravar();
     // $relatorio->deletar();
     $grupo->grafico->deletar();
+
+});
+
+#
+# Relatório em grupo - word
+#
+# POST http://localhost/acs/src/poms/relatorio/grupo/word
+# /poms/relatorio/grupo/word[14, 15, 18]
+App::$slim->get('/poms/relatorio/grupo/word/:ids', function ($ids) {
+    require "Grupo.php";    
+
+    $grupo = new Grupo;
+    $grupo = $grupo->factory(json_decode($ids));
+    // var_dump($grupo);
+
+    require "RelatorioGrupoWord.php";
+
+
+
+});
+
+#
+#
+#
+App::$slim->get('/poms/graficos/:nome_img', function ($nome_img) {
+    // require "Model.php";
+
+    $url = App::$path['base-dir'] . '/files-temp/' . "/570be7e8c2b03.png";
+    // var_dump($file);
+
+     if (file_exists($url)) {
+          header('Content-Description: File Transfer');
+          header('Content-Type: application/octet-stream');
+          header('Content-Disposition: inline; filename="'.basename($url).'"' );
+          header('Expires: 0');
+          header('Cache-Control: must-revalidate');
+          header('Pragma: public');
+          header('Content-Length: ' . filesize($url));
+          
+          readfile($url);
+          exit;
+      }
 
 });
 
